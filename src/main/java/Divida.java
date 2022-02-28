19559da463e4322da6e06f058bb3c3e31c0528e5
Robson <robmartinhao@gmail.com>
2022-02-28 12:28:55 -0300

    Revisitando a orientação a objetos

---
+++ src/main/java/Divida.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+
+public class Divida {
+
+    private double total;
+    private double valorPago;
+    private String credor;
+    private String cnpjCredor;
+    private ArrayList<Pagamento> pagamentos = new ArrayList<>();
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public double getValorPago() {
+        return valorPago;
+    }
+
+    public String getCredor() {
+        return credor;
+    }
+
+    public void setCredor(String credor) {
+        this.credor = credor;
+    }
+
+    public String getCnpjCredor() {
+        return cnpjCredor;
+    }
+
+    public void setCnpjCredor(String cnpjCredor) {
+        this.cnpjCredor = cnpjCredor;
+    }
+
+    public ArrayList<Pagamento> getPagamentos() {
+        return pagamentos;
+    }
+
+    private void paga(double valor) {
+        if (valor < 0) {
+            throw new IllegalArgumentException("Valor inválido para pagamento");
+        }
+        if (valor > 100) {
+            valor = valor -8;
+        }
+        this.valorPago += valor;
+    }
+
+    public void registra(Pagamento pagamento) {
+        this.pagamentos.add(pagamento);
+        this.paga(pagamento.getValor());
+    }
+}
---
+++ src/main/java/GerenciadorDeDividas.java
@@ -0,0 +1,9 @@
+public class GerenciadorDeDividas {
+    public void efetuaPagamento(Divida divida, double valor, String nomePagador, String cnpjPagador) {
+        Pagamento pagamento = new Pagamento();
+        pagamento.setCnpjPagador(cnpjPagador);
+        pagamento.setPagador(nomePagador);
+        pagamento.setValor(valor);
+        divida.registra(pagamento);
+    }
+}
